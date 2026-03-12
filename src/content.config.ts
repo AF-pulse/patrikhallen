ff5db424252d420b3b861c10eb97fe00fa0cd931
AF-pulse <patrik.hallen@dynaventures.se>
2026-03-12 19:46:50 +0100

    Add loader for insights collection in config

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,7 +1,9 @@
 import { defineCollection, z } from "astro:content";
+import { glob } from "astro/loaders";
 
 const insights = defineCollection({
-  type: "content",
+  loader: glob({ pattern: "**/*.md", base: "./src/content/insights" }),
+
   schema: z.object({
     title: z.string(),
     topic: z.string(),
